48f76656f91dfd9ee111b9dcd2a5e80eacf9b6d0
Bueltge <Bueltge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-28 13:23:40 +0000

    new tag
    git-svn-id: https://plugins.svn.wordpress.org/js-antispam/tags/2.1.1@578406 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inpsyde-antispam.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * Plugin Name: Inpsyde Antispam
+ * Plugin URI:  http://wordpress.org/extend/plugins/js-antispam/
+ * Description: Simple Antispam honeypot solution.
+ * Author:      Inpsyde GmbH
+ * Author URI:  http://inpsyde.com
+ * License:     GPLv3
+ * License URI: license.txt
+ * Version:     2.1.1
+ * Text Domain: inps-antispam
+ * Domain Path: /languages
+ */
+
+// check for right php version
+$correct_php_version = version_compare( phpversion(), '5.3.0', '>=' );
+
+if ( ! $correct_php_version ) {
+	echo 'Inpsyde Antispam Plugin requires <strong>PHP 5.3</strong> or higher.<br>';
+	echo 'You are running PHP ' . phpversion();
+	exit;
+}
+
+require_once( 'antispam.php' );
